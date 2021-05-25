f4ea82943cd9abce32d97be7ec54db31bf4b383f
Rahul <rahulismt491@gmail.com>
2021-05-26 01:14:24 +0545

    second commit

--- app/src/main/java/com/example/roomdatabase/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,10 +12,14 @@ import android.widget.EditText;
 import com.example.roomdatabase.Room.MyDatabase;
 import com.example.roomdatabase.Room.Student;
 
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText firstName,secondName,className;
-    Button insert;
+    EditText firstName,secondName,className,updatename,updateid,deleteid;
+    Button insert,read,update,delete;
+    MyDatabase myDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,17 @@ public class MainActivity extends AppCompatActivity {
         firstName=findViewById(R.id.firstName);
         secondName=findViewById(R.id.secondName);
         className=findViewById(R.id.className);
+        updatename=findViewById(R.id.updatename);
+        updateid=findViewById(R.id.updateid);
+        deleteid=findViewById(R.id.deleteID);
+
         insert=findViewById(R.id.insert);
+        read=findViewById(R.id.read);
+        update=findViewById(R.id.btnUpdate);
+        delete=findViewById(R.id.btnDelete);
+
+
+        SetUpDB();
 
 
         insert.setOnClickListener(new View.OnClickListener() {
@@ -35,12 +50,45 @@ public class MainActivity extends AppCompatActivity {
                         ,secondName.getText().toString()
                 ,className.getText().toString());
 
-                MyDatabase myDatabase=
-                        Room.databaseBuilder(MainActivity.this , MyDatabase.class ,"StudentDB")
-                                .allowMainThreadQueries().build();
+
                 myDatabase.dao().studentInsertion(student);
 
             }
         });
+
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+              List<Student> stuData=myDatabase.dao().getStudent();
+              for (int i=0;i<stuData.size();i++){
+                  Log.d("STUDENT_DATA" , String.valueOf(stuData.get(i).getStuId() +": "+
+                          stuData.get(i).getStuFirstName()+": "+ stuData.get(i).getStuLastName()+": "+
+                          stuData.get(i).getStuClass()));
+              }
+
+
+            }
+        });
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDatabase.dao().updatestu(updatename.getText().toString() ,
+                        Integer.parseInt(updateid.getText().toString()));
+            }
+        });
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                myDatabase.dao().delete(Integer.parseInt(deleteid.getText().toString()));
+            }
+        });
+
+    }
+    private void SetUpDB(){
+         myDatabase= Room.databaseBuilder(MainActivity.this , MyDatabase.class ,"StudentDB")
+                        .allowMainThreadQueries().build();
     }
 }
--- app/src/main/java/com/example/roomdatabase/Room/Dao.java
+++ app/src/main/java/com/example/roomdatabase/Room/Dao.java
@@ -2,10 +2,23 @@ package com.example.roomdatabase.Room;
 
 
 import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 @androidx.room.Dao
 public interface Dao {
 
 @Insert
     public void studentInsertion(Student student);
+
+    @Query("Select * from Student")
+    List<Student> getStudent();
+
+    @Query("Update Student set stuFirstName=:stuName where stuId=:stuID")
+    void updatestu(String stuName , int stuID);
+
+    @Query("Delete from Student Where stuID = :stuID")
+    void delete(int stuID);
+
 }
